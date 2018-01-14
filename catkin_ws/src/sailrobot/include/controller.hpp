57170e05865f11e76bf675a27e6cfe7d72a01451
Ulysse VAUTIER <ulysse.vautier@plymouth.ac.uk>
2018-01-14 21:15:29 +0000

    beginning controller class

--- catkin_ws/src/sailrobot/include/controller.hpp
+++ catkin_ws/src/sailrobot/include/controller.hpp
@@ -1,11 +1,15 @@
 #ifndef CONTROLLER_HPP
 #define CONTROLLER_HPP
 
+#include "ros/ros.h"
+
 namespace Sailboat{
 	class Controller{
 	public:
 		Controller(){}
 		~Controller(){}
+
+		virtual void control();
 	private:
 	};
 }
