00e81a6dffbdef907f837d922d85c35f76ecdb1f
Ulysse VAUTIER <ulysse.vautier@plymouth.ac.uk>
2018-03-23 14:45:06 +0000

    Adding utilities files

---
+++ catkin_ws/src/sailrobot/src/utilities.cpp
@@ -0,0 +1,21 @@
+#include <utilities.hpp>
+
+float Utility::GPSDist(float lat1, float long1, float lat2, float long2){
+
+}
+
+float Utility::GPSAngle(float lat1, float long1, float lat2, float long2){
+
+}
+
+float Utility::GPStoCartesian(float lat, float gpslong){
+
+}
+
+float Utility::CartesiantoGPS(float x, float y){
+
+}
+
+float Utility::RudderByCap(float theta){
+
+}
