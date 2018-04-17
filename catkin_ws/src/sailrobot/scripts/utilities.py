440889fa09a1a5ca0ffb99f135777df66be79dc2
uvautier <ulysse.vautier@plymouth.ac.uk>
2018-04-17 18:08:38 +0100

    hotfix and optimize

--- catkin_ws/src/sailrobot/scripts/utilities.py
+++ catkin_ws/src/sailrobot/scripts/utilities.py
@@ -3,34 +3,37 @@ import math
 
 def GPSDist(lat1, lon1, lat2, lon2):
 	R = 6371000
-        ksi1 = lat1*math.pi/180.0
-        ksi2 = lat2*math.pi/180.0
-        dksi = (lat2-lat1)*math.pi/180.0
-        dlambda = (lon2-lon1)*math.pi/180.0
+	degtorad = math.pi/180.0
+    ksi1 = lat1*degtorad
+	ksi2 = lat2*degtorad
+	dksi = (lat2-lat1)*degtorad
+	dlambda = (lon2-lon1)*degtorad
 
-        a = math.sin(dksi/2.0)*math.sin(dksi/2.0) + math.cos(ksi1)*math.cos(ksi2)*math.sin(dlambda/2.0)*math.sin(dlambda/2.0)
-        c = 2*math.atan2(sqrt(a), sqrt(1-a))
-        return R*c
+	a = math.sin(dksi/2.0)*math.sin(dksi/2.0) + math.cos(ksi1)*math.cos(ksi2)*math.sin(dlambda/2.0)*math.sin(dlambda/2.0)
+	c = 2*math.atan2(math.sqrt(a), math.sqrt(1-a))
+	return R*c
 
 def GPSDistFast(lat1, lon1, lat2, lon2):
-	ksi1 = lat1*math.pi/180.0
-        ksi2 = lat2*math.pi/180.0
-        lam1 = lon1*math.pi/180.0
-        lam2 = lon2*math.pi/180.0
+	degtorad = math.pi/180.0
+	ksi1 = lat1*degtorad
+	ksi2 = lat2*degtorad
+	lam1 = lon1*degtorad
+	lam2 = lon2*degtorad
 
-        x = (lam2-lam1)*math.cos((ksi1+ksi2)/2.0)
-        y = ksi2-ksi1
-        return math.sqrt(x*x + y*y) * 6371000
+	x = (lam2-lam1)*math.cos((ksi1+ksi2)/2.0)
+	y = ksi2-ksi1
+	return math.sqrt(x*x + y*y) * 6371000
 
 def GPSBearing(lat1, lon1, lat2, lon2):
-	ksi1 = lat1*math.pi/180.0
-        ksi2 = lat2*math.pi/180.0
-        lam1 = lon1*math.pi/180.0
-        lam2 = lon2*math.pi/180.0
+	degtorad = math.pi/180.0
+	ksi1 = lat1*degtorad
+	ksi2 = lat2*degtorad
+	lam1 = lon1*degtorad
+	lam2 = lon2*degtorad
 
-        y = math.sin(lam2-lam1)*math.cos(ksi2)
-        x = math.cos(ksi1)*math.sin(ksi2)-math.sin(ksi1)*math.cos(ksi2)*math.cos(lam2-lam1)
-        return math.atan2(y,x)
+	y = math.sin(lam2-lam1)*math.cos(ksi2)
+	x = math.cos(ksi1)*math.sin(ksi2)-math.sin(ksi1)*math.cos(ksi2)*math.cos(lam2-lam1)
+	return math.atan2(y,x)
 
 def QuaternionToEuler(w, x, y, z):
 	ysqr = y * y
