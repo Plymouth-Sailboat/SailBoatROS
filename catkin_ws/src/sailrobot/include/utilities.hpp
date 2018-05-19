84f5a69b61650153d81f081d346f077e4799c824
Ulysse Vautier <ulysse.vautier@plymouth.ac.uk>
2018-05-19 01:00:25 +0000

    potential_field (python) now uses utilities to read GPS file

--- catkin_ws/src/sailrobot/include/utilities.hpp
+++ catkin_ws/src/sailrobot/include/utilities.hpp
@@ -12,9 +12,9 @@ class Utility{
 		static float GPSDist(float lat1, float lon1, float lat2, float lon2);
 		static float GPSDist(glm::vec2 point1, glm::vec2 point2);
 		static float GPSDistFast(float lat1, float lon1, float lat2, float lon2);
-                static float GPSDistFast(glm::vec2 point1, glm::vec2 point2);		
+		static float GPSDistFast(glm::vec2 point1, glm::vec2 point2);		
 		static float GPSBearing(float lat1, float lon1, float lat2, float lon2);
-                static float GPSBearing(glm::vec2 point1, glm::vec2 point2);
+		static float GPSBearing(glm::vec2 point1, glm::vec2 point2);
 		static float GPStoCartesian(float lat, float gpslong);
 		static float CartesiantoGPS(float x, float y);
 		static glm::vec3 QuaternionToEuler(float x, float y, float z, float w);
