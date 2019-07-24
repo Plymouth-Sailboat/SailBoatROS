4e337298199bfedb7244eaec285bde416f32e389
Ulysse Vautier <ulysse.vautier@plymouth.ac.uk>
2019-07-24 12:25:55 +0100

    Fixed identification durations and steps

--- sailrobot/src/identification_node/identification.cpp
+++ sailrobot/src/identification_node/identification.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <utilities.hpp>
 #include <ros/package.h>
+#include <nlopt.hpp>
 
 using namespace Sailboat;
 using namespace glm;
@@ -30,8 +31,6 @@ geometry_msgs::Twist Identification::control(){
 	geometry_msgs::Twist cmd;
 
 	vec2 current(gpsMsg.latitude, gpsMsg.longitude);
-	vec2 currentRad = vec2(gpsMsg.latitude*M_PI/180.0, gpsMsg.longitude*M_PI/180.0);
-	vec3 currentXYZ = Utility::GPSToCartesian(gpsMsg.latitude, gpsMsg.longitude);
 
 	vec3 heading = Utility::QuaternionToEuler(imuMsg.orientation);
 	float currentHeading = heading.z;
@@ -42,57 +41,50 @@ geometry_msgs::Twist Identification::control(){
 	float thetabar;
 	switch(step){
 		case 0:{
-			       vec3 b = Utility::GPSToCartesian(goal1);
-			       vec3 a = Utility::GPSToCartesian(initPos);
-			       float na = glm::length(a);
-			       float nb = glm::length(b);
+			float lat1 = initPos.x;
+			float lat2 = goal1.x;
+			float long1 = initPos.y;
+			float long2 = goal1.y;
 
-			       float lat1 = initPos.x*M_PI/180.0;
-			       float lat2 = goal1.x*M_PI/180.0;
-			       float long1 = initPos.y*M_PI/180.0;
-			       float long2 = goal1.y*M_PI/180.0;
+			double diflat = lat2-lat1;
+			double diflong = long2 - long1;
+			double leng = (diflat*(current.x-lat1)+diflong*(current.y-long1))/(diflat*diflat+diflong*diflong);
+			vec2 currline(lat1+diflat*leng,long1+diflong*leng);
 
-			       double diflat = lat2-lat1;
-			       double diflong = long2 - long1;
-			       double leng = (diflat*(currentRad.x-lat1)+diflong*(currentRad.y-long1))/(diflat*diflat+diflong*diflong);
-			       vec2 currline(lat1+diflat*leng,long1+diflong*leng);
+			double distToLine = Utility::GPSDist(currline,current);
 
-			       double distToLine = Utility::GPSDist(currline,current);
+			float b = Utility::GPSBearing(initPos,current);
 
-			       vec3 n = glm::cross(a,b)/(na*nb);
+			float phi = Utility::GPSBearing(initPos,goal1);
+			float e = sin(b-phi)>0?distToLine:-distToLine;
+			thetabar = phi - 2*M_PI/3.0/M_PI*atan(e/10);
 
-			       float e = glm::dot(currentXYZ,n);
-			       e = e>0?distToLine:-distToLine;
+			ruddersail = Utility::StandardCommand(heading,thetabar, windNorth, M_PI/6.0);
+			//ruddersail = Utility::StandardCommand(heading,initWind+(float)(M_PI/2.0), windNorth, (float)(M_PI/6.0));
+			double duration  = ros::Time::now().toSec() - start;
 
-			       float phi = Utility::GPSBearing(initPos,goal1);
-			       thetabar = phi - 2*M_PI/3.0/M_PI*atan(e/10);
-
-			       //ruddersail = Utility::StandardCommand(currentHeading,thetabar, windNorth, M_PI/6.0);
-			       ruddersail = Utility::StandardCommand(heading,initWind+(float)(M_PI/2.0), windNorth, (float)(M_PI/6.0));
-				double duration  = ros::Time::now().toSec() - start;
-
-			       if(cos(currentHeading - (initWind+M_PI/2.0)) > 0.7 && duration > 60){
-				       step++;
-				       start = ros::Time::now().toSec();
-			       }
-			       break;
-		       }
+			if(cos(currentHeading - (initWind+M_PI/2.0)) > 0.7 && duration > 60){
+				step++;
+				start = ros::Time::now().toSec();
+			}
+			break;
+		}
 		case 1:{
-			       ruddersail = Utility::StandardCommand(heading,initWind+(float)(3.0*M_PI/4.0), windNorth, (float)(M_PI/2.0));
-				double duration  = ros::Time::now().toSec() - start;
+			ruddersail = Utility::StandardCommand(heading,initWind+(float)(3.0*M_PI/4.0), windNorth, (float)(M_PI/2.0));
+			double duration  = ros::Time::now().toSec() - start;
 
-			       if(cos(currentHeading - (initWind+3.0*M_PI/4.0)) > 0.9 && duration > 30){
-				       step++;
-				       start = ros::Time::now().toSec();
-			       }
-			       break;
-		       }
+			if(cos(currentHeading - (initWind+3.0*M_PI/4.0)) > 0.9 && duration > 10){
+				step++;
+				start = ros::Time::now().toSec();
+			}
+			break;
+		}
 		case 2:{
 			       ruddersail.x = M_PI/4.0;
 			       ruddersail.y = 0;
-				double duration  = ros::Time::now().toSec() - start;
+			       double duration  = ros::Time::now().toSec() - start;
 
-			       if(duration > 10){
+			       if(duration > 5){
 				       step++;
 				       start = ros::Time::now().toSec();
 			       }
@@ -101,9 +93,9 @@ geometry_msgs::Twist Identification::control(){
 		case 3:{
 			       ruddersail.x = 0;
 			       ruddersail.y = 0;
-				double duration  = ros::Time::now().toSec() - start;
+			       double duration  = ros::Time::now().toSec() - start;
 
-			       if(duration > 10){
+			       if(duration > 5){
 				       step++;
 				       start = ros::Time::now().toSec();
 			       }
@@ -111,15 +103,15 @@ geometry_msgs::Twist Identification::control(){
 		       }
 	}
 
-
-	data << std::to_string(ros::Time::now().toSec()) << "," << std::to_string(ros::Time::now().toSec() - start) << "," << std::to_string(step) << "," << std::to_string(velMsg.linear.x) << "," << std::to_string(velMsg.linear.y) << "," << std::to_string(velMsg.linear.z) << "," << std::to_string(imuMsg.linear_acceleration.x) << "," << std::to_string(imuMsg.linear_acceleration.y) << "," << std::to_string(imuMsg.linear_acceleration.z) << "," << std::to_string(currentHeading) << "," << std::to_string(windNorth) << "," << std::to_string(ruddersail.x) << "," << std::to_string(ruddersail.y) << "," << std::to_string(current.x) << "," << std::to_string(current.y) << std::endl; 
+	if(step < 4)
+		data << std::to_string(ros::Time::now().toSec()) << "," << std::to_string(ros::Time::now().toSec() - start) << "," << std::to_string(step) << "," << std::to_string(velMsg.linear.x) << "," << std::to_string(velMsg.linear.y) << "," << std::to_string(velMsg.linear.z) << "," << std::to_string(imuMsg.linear_acceleration.x) << "," << std::to_string(imuMsg.linear_acceleration.y) << "," << std::to_string(imuMsg.linear_acceleration.z) << "," << std::to_string(currentHeading) << "," << std::to_string(windNorth) << "," << std::to_string(ruddersail.x) << "," << std::to_string(ruddersail.y) << "," << std::to_string(current.x) << "," << std::to_string(current.y) << std::endl; 
 
 	cmd.angular.x = (double)ruddersail.x;
 	cmd.angular.y = (double)ruddersail.y;
 
 	std::string message = "";
 
-	publishMSG("step " + std::to_string(step));
+	publishLOG("step " + std::to_string(step));
 	return cmd;
 }
 
